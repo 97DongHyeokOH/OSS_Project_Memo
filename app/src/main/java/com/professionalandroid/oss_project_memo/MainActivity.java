1d5cb241ac20ef5f982eda09ab06fc56bee0ae98
DongHyeok OH <donggar97@naver.com>
2020-06-27 00:02:45 +0900

    메모 추가를 누르면 새로운 메모 입력창으로 이동. 저장을 누르면 다시 메인화면으로 돌아옴

--- app/src/main/java/com/professionalandroid/oss_project_memo/MainActivity.java
+++ app/src/main/java/com/professionalandroid/oss_project_memo/MainActivity.java
@@ -2,13 +2,40 @@ package com.professionalandroid.oss_project_memo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView list;
+    private Button add;
+    private Button delete;
+    private String str_title;
+    private String str_content;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        list = findViewById(R.id.list);
+        add = findViewById(R.id.add);
+        delete = findViewById(R.id.delete);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NewMemoActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
